55820a1887021adb47c85e2c272f168892973fed
mega05 <benape05@gmail.com>
2024-12-28 19:07:25 -0800

    Initial commit

---
+++ src/main/java/com/ben/megaswoodworkingmod/MegasWoodworking.java
@@ -0,0 +1,15 @@
+package com.ben.megaswoodworkingmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MegasWoodworking implements ModInitializer {
+	public static final String MOD_ID = "megas-woodworking";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/ben/megaswoodworkingmod/WoodworkingModClient.java
@@ -0,0 +1,10 @@
+package com.ben.megaswoodworkingmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WoodworkingModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
